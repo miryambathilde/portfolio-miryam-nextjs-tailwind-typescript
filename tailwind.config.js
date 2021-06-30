2c2b8595c6be88d96d873aca88d8869fc79c958d
Miryam Bathilde <mbathilde@gmail.com>
2021-06-30 10:58:41 +0200

    añado tailwind css a global css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  purge: [
-    './pages/**/*.tsx', './components/**/*.tsx'
-  ],
+  purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -10,4 +8,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
