bff0d5f79f22dca2537490f238bda9c1c22efa1a
Miryam Bathilde <mbathilde@gmail.com>
2021-06-30 12:59:04 +0200

    cambio foto de sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,7 +2,7 @@ const Sidebar = () => {
   return (
     <div>
       <img
-        src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=693&q=80"
+        src="https://images.unsplash.com/photo-1579403124614-197f69d8187b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2000&q=80"
         alt="user avatar"
       />
       <h3>
@@ -10,8 +10,23 @@ const Sidebar = () => {
         Bathilde
         <span>Crevillen</span>
       </h3>
-      <p>Web developer</p>
-      <p>Download Resume</p>
+      <p>Desarrolladora Web</p>
+      <p>Descargar CV</p>
+
+      {/* //social icons */}
+
+      {/* address */}
+      <div>
+        <div>
+          <span>Sevilla, España</span>
+        </div>
+        <p>mbathilde@gmail.com</p>
+        <p>+34 664 405 691</p>
+      </div>
+      {/* email button */}
+      <button>Contactar por email</button>
+      <button>Cambiar el tema</button>
+
     </div>
   );
 };
