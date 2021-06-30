5af03ac9e8fef416112c267f6e0c2dbe9f410e0e
Miryam Bathilde <mbathilde@gmail.com>
2021-06-30 11:22:09 +0200

    diseño de body y grid de sidebar - responsive

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css';
+import "../styles/globals.css";
 //import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 ">
+      <div className="lg:col-span-3 bg-white rounded-2xl col-span-12">
+        Sidebar
+      </div>
+      <div className="lg:col-span-9 bg-white rounded-2xl col-span-12">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
