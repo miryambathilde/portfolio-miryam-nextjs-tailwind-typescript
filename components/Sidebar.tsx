ad5e67e3a78462ab106afdddf2dc67268dc3af9c
Miryam Bathilde <mbathilde@gmail.com>
2021-06-30 14:59:04 +0200

    añado fuente personalizada + color de fondo

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -14,7 +14,7 @@ const Sidebar = () => {
         alt="user avatar"
         //className="w-32 h-32 mx-auto rounded-full"
       />
-      <h3 className="my-4 text-3xl font-medium tracking-wider">
+      <h3 className="my-4 text-3xl font-medium tracking-wider font-kaushan">
         <span>Miryam</span>
         <br />
         Bathilde
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@ module.exports = {
   purge: ["./pages/**/*.tsx", "./components/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    /* fonts sans + kaushan de google fonts */
+    fontFamily: {
+      /* sans: ["ui-sans-serif", "system-ui"], */
+      kaushan: ["Kaushan Script"],
+    },
+    extend: {
+      /* custome colores */
+      colors: {
+        green: {
+          DEFAULT: '#34D399',
+        }
+      }
+    },
   },
   variants: {
     extend: {},
