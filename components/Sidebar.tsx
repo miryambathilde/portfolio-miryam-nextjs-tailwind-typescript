e201236abeb62e92a607f68da355c49f12e79e60
Miryam Bathilde <mbathilde@gmail.com>
2021-06-30 11:43:04 +0200

    sidebar component layout + import to app

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,19 @@
 const Sidebar = () => {
   return (
     <div>
-      
+      <img
+        src="https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=693&q=80"
+        alt="user avatar"
+      />
+      <h3>
+        <span>Miryam</span>
+        Bathilde
+        <span>Crevillen</span>
+      </h3>
+      <p>Web developer</p>
+      <p>Download Resume</p>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
