173dd91946eee146aa92c8ad39cf0496405e82f9
Miryam Bathilde <mbathilde@gmail.com>
2021-07-01 11:17:54 +0200

    creo data ts para services + type para interface

---
+++ data.ts
@@ -0,0 +1,16 @@
+import {RiComputerLine} from 'react-icons/ri'
+import { iService } from './type';
+
+export const services:iService[] = [
+
+  {
+    title:'Desarrollo Frontend',
+    about:'lorem....',
+    Icon: RiComputerLine,
+  },
+  {
+    title: "title",
+    about:'lorem....',
+    Icon: RiComputerLine,
+  },
+]
---
+++ type.ts
@@ -0,0 +1,8 @@
+import { IconType } from "react-icons";
+
+//interface is the shape of the object
+export interface iService {
+  title: string,
+  about: string,
+  Icon: IconType
+}
