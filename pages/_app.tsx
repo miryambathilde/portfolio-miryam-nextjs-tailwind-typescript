c5fc77c9bcf36c943357cf3afe7f1b5c8dcdccfa
Miryam Bathilde <mbathilde@gmail.com>
2021-07-02 12:14:45 +0200

    añadimos themeProvider a app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,23 +1,27 @@
 import Navbar from "../components/Navbar";
 import Sidebar from "../components/Sidebar";
 import "../styles/globals.css";
+//themeProvider - next themes
+import { ThemeProvider } from "next-themes";
 //import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <div className="grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-32 md:px-42">
-      <div className="col-span-12 p-4 text-center bg-white dark:bg-gray-800 lg:col-span-3 rounded-2xl">
-        {/* sidebar */}
-        <Sidebar />
-      </div>
-      <div className="flex flex-col col-span-12 overflow-hidden bg-white lg:col-span-9 rounded-2xl">
-        {/* navbar */}
-        <Navbar />
-        {/* about */}
-        <Component {...pageProps} />
-      </div>
-    </div>
-  );
+	return (
+		<ThemeProvider attribute='class'>
+			<div className='grid grid-cols-12 gap-6 px-5 lg:px-48 my-14 sm:px-32 md:px-42'>
+				<div className='col-span-12 p-4 text-center bg-white dark:bg-gray-800 lg:col-span-3 rounded-2xl'>
+					{/* sidebar */}
+					<Sidebar />
+				</div>
+				<div className='flex flex-col col-span-12 overflow-hidden bg-white lg:col-span-9 rounded-2xl'>
+					{/* navbar */}
+					<Navbar />
+					{/* about */}
+					<Component {...pageProps} />
+				</div>
+			</div>
+		</ThemeProvider>
+	);
 }
 
 export default MyApp;
