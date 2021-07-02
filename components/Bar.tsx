cf11e95d754ebb34aa71e16a6791b2b29ec363b0
Miryam Bathilde <mbathilde@gmail.com>
2021-07-02 11:53:46 +0200

    final layout + structure + icons resume page

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -6,7 +6,7 @@ const Bar: FunctionComponent<{
 }> = ({ data: { Icon, level, name } }) => {
 	return (
 		<div className="my-2 text-white bg-gray-300 rounded-full">
-			<div className='flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-500 via-green-400 to-blue-500'
+			<div className='flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-600 via-green-400 to-blue-500'
       style={{width: level}}
       
       >
--- pages/resume.tsx
+++ pages/resume.tsx
@@ -7,13 +7,13 @@ const resume = () => {
 			{/* //education & experience// */}
 			<div className='grid gap-10 md:grid-cols-2 xl:grid-cols-2'>
 				<div>
-					<h5 className='my-3 text-2xl font-bold'>Estudios</h5>
+					<h5 className='my-3 text-2xl font-bold text-gray-600'>Estudios</h5>
 					<div>
 						<h5 className='my-2 text-xl font-bold'>
-							Bootcamp Full Stack Web Developer: Opensource
+						Codehouse Academy (2021)
 						</h5>
-						<p className='font-semibold'>Codehouse Academy (2021)</p>
-						<p className='my-3'>
+						<p className='font-semibold'>Bootcamp Full Stack Web Developer: Opensource</p>
+						<p className='my-3 text-justify'>
 							En el Bootcamp de Fullstack Web Developer Opensource, he adquirido
 							todos los conocimientos necesarios para el desarrollo completo de
 							una web, desde la parte que visualiza el cliente hasta la que
@@ -23,15 +23,55 @@ const resume = () => {
 							la actualidad por las empresas de desarrollo de software.
 						</p>
 					</div>
-				</div>
-				<div>
-					<h5 className='my-3 text-2xl font-bold'>Experiencia profesional</h5>
 					<div>
 						<h5 className='my-2 text-xl font-bold'>
-							Presentación del proyecto final de Bootcamp
+						Pixelpro (2020 - 2021)
 						</h5>
-						<p className='font-semibold'>Codehouse Academy (2021)</p>
+						<p className='font-semibold'>Estudiante carrera FullStack Developer</p>
 						<p className='my-3'>
+							HTML5, CSS3, Javascript avanzado, ECMAScript, Angular, React,
+							Angular con NodeJS.
+						</p>
+					</div>
+					<div>
+						<h5 className='my-2 text-xl font-bold'>
+						Bottega Career Services (2020)
+						</h5>
+						<p className='font-semibold'>Digital Literacy - Coding Foundations</p>
+						<p className='my-3 text-justify'>
+							1. - Fundamentos digitales y tendencias de la industria
+							tecnológica Conocimiento del vocabulario y conceptos básicos de
+							alfabetización digital. 
+							<br />
+							2. - Fundamentos HTML y CSS Los lenguajes
+							HTML y CSS para programar sitios web básicos que integran el uso
+							de estilos. 
+							<br />
+							3. - Fundamento de JavaScript Aprender e implementar
+							JavaScript para agregar características móviles a proyectos de
+							programación básicos. Fundamentos de la línea de comandos Cómo
+							usar el terminal y la línea de comandos para completar las tareas
+							de programación asignadas. Habilidades de desarrollo Entender las
+							tendencias actuales en la industria tecnológica y las posibles
+							opciones profesionales asociadas. Identificar estrategias de
+							gestión para liderar con eficacia proyectos, reuniones,
+							presentaciones y actividades asociadas en entornos de la industria
+							tecnológica. Reconocer las competencias de trabajo en equipo que
+							promueven la comunicación productiva, la colaboración y la
+							negociación.
+						</p>
+					</div>
+				</div>
+				<div>
+					<h5 className='my-3 text-2xl font-bold text-gray-600'>
+						Experiencia profesional
+					</h5>
+					<div>
+						<h5 className='my-2 text-xl font-bold'>
+						Codehouse Academy (2021)
+						</h5>
+						<p className='font-semibold'>Presentación del proyecto final de Bootcamp</p>
+						<p className='my-3 text-justify'>
 							Diseño, planificación e implementación de un proyecto real.
 							<br />
 							Aplicación de metodologías ágiles en el proyecto.
@@ -47,16 +87,18 @@ const resume = () => {
 
 			<div className='grid gap-6 md:grid-cols-2'>
 				<div>
-					<h5 className='my-3 text-2xl font-bold'>Lenguajes</h5>
-					<div className='my-2'>
+					<h5 className='my-4 text-2xl font-bold text-gray-600'>Lenguajes</h5>
+					<div className='my-4'>
 						{languages.map((language) => (
 							<Bar data={language} key={language.name} />
 						))}
 					</div>
 				</div>
 				<div>
-					<h5 className='my-3 text-2xl font-bold'>Herramientas & Frameworks</h5>
-					<div className='my-2'>
+					<h5 className='my-4 text-2xl font-bold text-gray-600'>
+						Herramientas & Frameworks
+					</h5>
+					<div className='my-4'>
 						{tools.map((tool) => (
 							<Bar data={tool} key={tool.name} />
 						))}
