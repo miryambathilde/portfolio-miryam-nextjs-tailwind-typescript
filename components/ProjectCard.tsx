270934872ba6aa76db8adbb89cde7191efa84566
Miryam Bathilde <mbathilde@gmail.com>
2021-07-03 15:52:17 +0200

    finished layout showDetail

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -19,42 +19,63 @@ const ProjectCard: FunctionComponent<{
 	const [showDetail, setShowDetail] = useState(false);
 	return (
 		<div>
-			<img src={image_path} alt={name} className='cursor-pointer' />
+			<img src={image_path} alt={name} className='cursor-pointer' onClick={()=>setShowDetail(true)}/>
 			<p className='my-2 text-center dark:text-gray-100'>{name}</p>
 
-			<div className='grid md:grid-cols-2 xl:grid-cols-2'>
-				<div>
-					<img src={image_path} alt={name} />
-					<div>
-						<a href={github_url}>
-							<AiFillGithub size={20} />{" "}
-							<span className='dark:text-gray-100'>GitHub</span>
-						</a>
-						<a href={github_url}>
-							<AiFillProject size={20} />{" "}
-							<span className='dark:text-gray-100'>Project</span>
-						</a>
-					</div>
-				</div>
-			</div>
+			{showDetail && (
+        <div className="absolute top-0 left-0 z-10 grid w-full h-auto p-2 text-gray-800 bg-gray-200 md:grid-cols-2 gap-x-12 dark:text-white dark:bg-gray-500">
+          <div>
+            {/* <img src={image_path} alt={name} /> */}
 
-			<div className='dark:text-gray-100'>
-				<h2>{name}</h2>
-				<h3>{description}</h3>
-				<div className='dark:text-gray-100'>
-					{key_techs.map((tech) => (
-						<span key={tech}>{tech}</span>
-					))}
-				</div>
+            {/* <Image
+              src={image_path}
+              alt={name}
+              layout="responsive"
+              height="150"
+              width="300"
+            /> */}
+            <div className="flex justify-center my-4 space-x-3">
+              <a
+                href={github_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg text-gray-100 bg-gray-500 dark:bg-gray-300 dark:text-gray-700"
+              >
+                <AiFillGithub /> <span>Github</span>
+              </a>
+              <a
+                href={deployed_url}
+                className="flex items-center px-4 py-2 space-x-3 text-lg text-gray-100 bg-gray-500 dark:bg-gray-300 dark:text-gray-700"
+              >
+                <AiFillProject /> <span>Project</span>
+              </a>
+            </div>
+          </div>
 
-				<div className='dark:text-gray-100'>
-					<button>
-						<AiFillCloseCircle size={30} />
-					</button>
-				</div>
-			</div>
-		</div>
-	);
+          <div>
+            <h2 className="mb-3 text-xl font-medium md:text-2xl">{name}</h2>
+            <h3 className="mb-3 font-medium">{description}</h3>
+
+            <div className="flex flex-wrap mt-5 space-x-2 text-sm font-semibold">
+              {key_techs.map((tech) => (
+                <span
+                  key={tech}
+                  className="px-2 py-1 my-1 text-gray-100 bg-gray-500 dark:bg-gray-200 dark:text-gray-700 rounde-sm"
+                >
+                  {tech}
+                </span>
+              ))}
+            </div>
+          </div>
+
+          <button
+            onClick={() => setShowDetail(false)}
+            className="absolute p-1 bg-gray-200 rounded-full top-3 right-3 focus:outline-none dark:bg-gray-500"
+          >
+            <AiFillCloseCircle size={30} />
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ProjectCard;
