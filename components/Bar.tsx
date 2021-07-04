95f130884ea67d5560e7e6cef24821c0d4217b4c
Miryam Bathilde <mbathilde@gmail.com>
2021-07-04 22:17:53 +0200

    implementing framer-animation

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -8,13 +8,23 @@ const Bar: FunctionComponent<{
 	const bar_width = `${level}%`
 
 	const variants = {
-		
+		initial: {
+			with: 0
+		},
+		animate: {
+			width:bar_width,
+			transition: {
+				duration: 0.4,
+			},
+		},
 	}
 	return (
 		<div className='my-2 font-bold text-white bg-gray-400 rounded-full dark:bg-gray-50'>
 			<motion.div
 				className='flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-600 via-green-400 to-blue-500 dark:from-green-600 dark:via-green-400 dark:to-blue-400'
-				style={{ width: bar_width }}
+				style={{ 
+					width: bar_width 
+				}}
 				variants={variants}
 				initial="initial"
 				animate="animate"
