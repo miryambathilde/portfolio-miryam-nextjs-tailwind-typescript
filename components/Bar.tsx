6350b67804dfc5725ec26dfa3beaaa593aac9c9b
Miryam Bathilde <mbathilde@gmail.com>
2021-07-04 22:08:39 +0200

    install framer-motion

--- components/Bar.tsx
+++ components/Bar.tsx
@@ -4,6 +4,11 @@ import { Skill } from "../type";
 const Bar: FunctionComponent<{
 	data: Skill;
 }> = ({ data: { Icon, level, name } }) => {
+	const bar_width = `${level}%`
+
+	const variants = {
+		
+	}
 	return (
 		<div className="my-2 font-bold text-white bg-gray-400 rounded-full dark:bg-gray-50">
 			<div className='flex items-center px-4 py-1 rounded-full bg-gradient-to-r from-green-600 via-green-400 to-blue-500 dark:from-green-600 dark:via-green-400 dark:to-blue-400'
